3b55a7360933569bb344d3bb963e204d91a63e42
tigergiangnguyen <tigernguyen5@yahoo.com>
2023-02-27 15:58:31 -0500

    Removed starter comments and clean up codes

--- assets/script.js
+++ assets/script.js
@@ -1,65 +1,44 @@
+// Global variables
 var dateDisplay = $("#currentDay");
 var current = dayjs().format("MMM DD, YYYY h:mm:ss a");
 
-
-// var save = document.querySelector(".saveBtn");
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
+// Saves user text input by clicking the save button that matches the textarea
 $(document).ready(
   $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
     $(".saveBtn").click(function () {
       var text = $(this).siblings(".description").val();
       var parentId = $(this).parent().attr("id");
-      
-    localStorage.setItem(parentId, text);
-
-      
+      localStorage.setItem(parentId, text);
     });
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   var currentHour = dayjs().hour();
 
-  function timeTracker() {
-    $(".time-block").each(function() {
-      const myArray = $(this).attr("id").split("hour-");
+function timeTracker() {
+  // Takes each array and splits to grab only the integer
+  $(".time-block").each(function() {
+    const myArray = $(this).attr("id").split("hour-");
 
-      if (currentHour > myArray[1]) {
-        $(this).addClass("past");
-        $(this).removeClass("present");
-        $(this).removeClass("future");
-      } 
+    // Replacing class in each div to match live time
+    if (currentHour > myArray[1]) {
+      $(this).addClass("past");
+      $(this).removeClass("present");
+      $(this).removeClass("future");
+    } 
       
-      else if (currentHour == myArray[1]) {
-        $(this).addClass("present");
-        $(this).removeClass("past");
-        $(this).removeClass("future");
-      }
+    else if (currentHour == myArray[1]) {
+      $(this).addClass("present");
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+    }
     
-      else {
-        $(this).addClass("future");
-        $(this).removeClass("past");
-        $(this).removeClass("present");
+    else {
+      $(this).addClass("future");
+      $(this).removeClass("past");
+      $(this).removeClass("present");
       }
     })
   };
 
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+// Saved user input in localStorage
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
 $("#hour-11 .description").val(localStorage.getItem("hour-11"));
@@ -70,10 +49,9 @@ $("#hour-15 .description").val(localStorage.getItem("hour-15"));
 $("#hour-16 .description").val(localStorage.getItem("hour-16"));
 $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-
-  // TODO: Add code to display the current date in the header of the page.
-  dateDisplay.text(current);
-setInterval(function () {
+// Runs a clock that updates each second live
+dateDisplay.text(current);
+  setInterval(function () {
   current = dayjs().format("MMM DD, YYYY h:mm:ss a");
   dateDisplay.text(current);
   timeTracker();
