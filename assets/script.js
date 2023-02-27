40c90317c3e871d0b2b27d8c1c270ff1ad25c735
tigergiangnguyen <tigernguyen5@yahoo.com>
2023-02-27 15:46:44 -0500

    Saved user input in localStorage

--- assets/script.js
+++ assets/script.js
@@ -60,6 +60,16 @@ $(document).ready(
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
 
   // TODO: Add code to display the current date in the header of the page.
   dateDisplay.text(current);
